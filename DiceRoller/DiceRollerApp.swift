e4147aa72df1a9d7515b0976ad68beb17bb7171a
luccadias <luccadiass@gmail.com>
2024-05-14 19:42:56 -0300

    feat: add project structure initial

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Lucca Dias on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
